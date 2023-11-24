acf020c69fc40d88c7ffeb264c047b8fa0255fc3
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-11-24 08:52:29 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMembers.Models
+{
+
+    interface IProduct
+    {
+        public void ShowProductDetails();
+    }
+
+    public class Product : IProduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public enum Brand
+        {
+            Nike = 0,
+            Addidas = 1,
+            Fendi = 3,
+            Gucci = 4
+        }
+
+        public Brand productBrand { get; set; }
+        public Product(string name, string desc, double price, Brand brand)
+        {
+            Name = name;
+            Description = desc;
+            Price = price;
+            productBrand = brand;
+        }
+
+        public void ShowProductDetails()
+        {
+            Console.WriteLine($"Product Name: {Name}");
+            Console.WriteLine($"Product Description: {Description}");
+            Console.WriteLine($"Product Price: {Price}");
+            Console.WriteLine($"Product Brand: {productBrand}");
+        }
+
+
+        //destructor
+        ~Product() {
+            Console.WriteLine("Destructor Called!");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassMembers.Models;
+
+namespace ClassMembers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product newProduct = new Product("Shoe", "A size 42 shoe", 32000.99, Product.Brand.Addidas);
+            newProduct.ShowProductDetails();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("---- Record -----");
+            //Implementing Record
+            Student newStudent = new Student("Ben", "Franklin", 20);
+            Console.WriteLine($"Student First Name: {newStudent.FirstName}");
+            Console.WriteLine($"Student Last Name: {newStudent.LastName}");
+            Console.WriteLine($"Student Age: {newStudent.Age}");
+
+        }
+    }
+
+    public record Student(string? FirstName, string? LastName, int? Age);
+
+}
